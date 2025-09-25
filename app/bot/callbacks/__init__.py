fff4b7d79c4e89ef90fbecf20f6d79663ef4a54f
Felipe Joaquim <167233033+felipe-fjs@users.noreply.github.com>
2025-09-25 10:21:42 -0300

    Update __init__.py
    adicionando "menu anterior" à função return_menu

--- app/bot/callbacks/__init__.py
+++ app/bot/callbacks/__init__.py
@@ -2,12 +2,13 @@ from telebot import types
 from app.bot import bot
 
 
-def return_menu(buttons: types.InlineKeyboardMarkup, call: types.CallbackQuery):
+def return_menu(buttons: types.InlineKeyboardMarkup, call: types.CallbackQuery, previous_menu: str = None):
     "Adiciona o botôes de 'menu inicial'"
     # "Adiciona os botôes de 'menu inicial' e 'menu anterior'"
 
     buttons.row(types.InlineKeyboardButton("Menu inicial", callback_data="primary_menu"))
-    # buttons.row(types.InlineKeyboardButton("Menu anterior", callback_data=call.data))
+    if previous_menu:
+        buttons.row(types.InlineKeyboardButton("Menu anterior", callback_data=previous_menu))
 
 def delete_message(call: types.CallbackQuery):
     bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
